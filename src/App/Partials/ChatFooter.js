576d36d7c40ea314f05ec406830a2094268c51a0
faz919 <faizit9@gmail.com>
2022-03-31 18:05:09 +0300

    sending messages

--- src/App/Partials/ChatFooter.js
+++ src/App/Partials/ChatFooter.js
@@ -1,38 +1,52 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import {Button, Input} from 'reactstrap'
 import * as FeatherIcon from 'react-feather'
-import WomenAvatar5 from "../../assets/img/women_avatar5.jpg";
-import {useDispatch, useSelector} from "react-redux";
+import WomenAvatar5 from "../../assets/img/women_avatar5.jpg"
+import {useDispatch, useSelector} from "react-redux"
+import { AuthContext } from '../../providers/AuthProvider'
+import { addDoc, collection, doc, updateDoc, getFirestore, Timestamp } from 'firebase/firestore'
+import app from '../../firebase'
 
-function ChatFooter(props) {
+const db = getFirestore(app)
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        props.onSubmit({
-            name: 'Mirabelle Tow',
-            avatar: <figure className="avatar">
-                <img src={WomenAvatar5} className="rounded-circle" alt="avatar"/>
-            </figure>,
-            text: props.inputMsg,
-            date: 'Now',
-            type: 'outgoing-message'
+function ChatFooter() {
+
+    const { user } = useContext(AuthContext)
+
+    const {selectedChat} = useSelector(state => state)
+
+    const [message, setMessage] = useState('');
+
+    const sendMessage = async (e) => {
+      e.preventDefault();
+  
+      const msg = message
+      setMessage('')
+  
+      const { uid } = user;
+  
+      await addDoc(collection(db, "chat-rooms", selectedChat.chat.id, "chat-messages"), {
+        img: null,
+        msg,
+        timeSent: Timestamp.fromDate(new Date()),
+        userID: uid,
+      }).then(() => {
+        updateDoc(doc(db, "chat-rooms", selectedChat.chat.id), {
+          latestMessage: msg,
+          latestMessageTime: Timestamp.fromDate(new Date())
         })
-    };
-
-    const handleChange = (e) => {
-        props.onChange(e.target.value)
-    };
+      })
+    }
 
     return (
         <div className="chat-footer">
-            <form onSubmit={handleSubmit}>
+            <form>
                 <div>
                     <Button color="light" className="mr-3" title="Emoji">
                         <FeatherIcon.Smile/>
                     </Button>
                 </div>
-                <Input type="text" className="form-control" placeholder="Write a message..." value={props.inputMsg}
-                       onChange={handleChange}/>
+                <Input type="text" className="form-control" placeholder="Write a message..." value={message} onChange={(e) => setMessage(e.target.value)}/>
                 <div className="form-buttons">
                     <Button color="light">
                         <FeatherIcon.Paperclip/>
@@ -40,7 +54,7 @@ function ChatFooter(props) {
                     <Button color="light" className="d-sm-none d-block">
                         <FeatherIcon.Mic/>
                     </Button>
-                    <Button color="primary">
+                    <Button color="primary" onClick={sendMessage} disabled={!message}>
                         <FeatherIcon.Send/>
                     </Button>
                 </div>
